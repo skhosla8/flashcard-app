618a06980aa18c41a37115a607ab1948d931d4eb
skhosla8 <59849053+skhosla8@users.noreply.github.com>
2025-12-30 16:02:57 -0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import StudyMode from './study-mode/page';
 export default function Home() {
   return (
     <ReduxProvider>
-      <main className='w-screen h-screen bg-blue-500'>
+      <main>
         <StudyMode />
       </main>
     </ReduxProvider>
